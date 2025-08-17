28e43ac8d24bf1787515228bd3c4fff60d7854e2
Serghei Soloviov <thatmean@gmail.com>
2025-08-17 10:31:46 +0300

    rollback to start point

--- backend/internal/infrastructure/storage/migrations/sqlite/006_add_api_keys_prefix.down.sql
+++
@@ -1,6 +0,0 @@
--- Remove prefix column and index from api_keys table
-DROP INDEX IF EXISTS idx_api_keys_prefix;
-
--- Note: SQLite doesn't support DROP COLUMN directly
--- This would require table recreation in a real rollback scenario
--- For now, we'll leave the column as it doesn't break functionality
